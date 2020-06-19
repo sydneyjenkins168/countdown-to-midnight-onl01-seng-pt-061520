9010a339183fd72b1ed1bc0387190b8bcb29aeda
Sydney Jenkins <sydjenkins168@gmail.com>
2020-06-19 19:53:19 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,5 +8,13 @@ def countdown(seconds)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
+def countdown_with_sleep(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
+
   
